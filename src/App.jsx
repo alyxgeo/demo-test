965e343c1f677478f0b07b2ade361bb197705cae
alyxgeo <alyxgeo76@gmail.com>
2023-11-28 19:24:36 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const App = () => {
+
+
+  const family = [
+    {
+      id: 1,
+      name: 'peter',
+      spouce: 'mary',
+      children: "john",
+    },
+    {
+      id: 2,
+      name: 'mary',
+      spouce: 1,
+      children: "john",
+    },
+    {
+      id: 3,
+      name: 'john',
+      spouce: [4, 2],
+      children: 'jeena',
+    },
+    {
+      id: 4,
+      name: 'rose',
+      spouce: [3],
+      children: "jeena",
+    }
+  ]
+
+  family[2]?.spouce.map((id) => (
+    //console.log(id)
+    console.log(
+      family.find((fam) => fam.id === id)
+    )
+  ))
+
+
+  return (
+    <div className='bg-red-200'>
+      hello
+    </div>
+  )
+}
+
+export default App
